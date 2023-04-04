843db630895ecf52c658c2ca0016c446d6c672bb
roshni gajbhiye <rohitkamui@gmail.comm>
2023-04-05 05:19:46 +0530

    my changes

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
         <Router>
           <Header />
             <Routes>
-                <Route index element={<Home />}></Route>
+                <Route   path='/'  index element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+                {/* <Route path="/*" element={<h1>Error Page</h1>}></Route> */}
             </Routes>
         </Router>
     </div>
