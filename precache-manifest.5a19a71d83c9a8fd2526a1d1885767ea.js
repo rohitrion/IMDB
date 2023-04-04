421c9017ce1f0f865992b06c05e44502f1504f40
roshni gajbhiye <rohitkamui@gmail.comm>
2023-04-05 05:14:01 +0530

    Updates

---
+++ precache-manifest.5a19a71d83c9a8fd2526a1d1885767ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbb615a1ad07bba799fa",
+    "url": "/IMDB/static/css/main.17acae5c.chunk.css"
+  },
+  {
+    "revision": "cbb615a1ad07bba799fa",
+    "url": "/IMDB/static/js/main.cbb615a1.chunk.js"
+  },
+  {
+    "revision": "56901b5ac89bbb8e6aed",
+    "url": "/IMDB/static/css/1.292c4393.chunk.css"
+  },
+  {
+    "revision": "56901b5ac89bbb8e6aed",
+    "url": "/IMDB/static/js/1.56901b5a.chunk.js"
+  },
+  {
+    "revision": "f4d020db1b53a98e9a04",
+    "url": "/IMDB/static/js/runtime~main.f4d020db.js"
+  },
+  {
+    "revision": "064bca44d8a14bcb766a4a35dc9e9ccd",
+    "url": "/IMDB/index.html"
+  }
+];
